33f6874dd36a66fb0e9b3e6ea938199646d0091f
seenso <>
2021-11-30 08:17:32 -0900

    Completed rendering Task component

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (<Task text={task.text} category={task.category}/>))}
+      {/* COMPONENTS MUST BE WRAPPED IN () */}
     </div>
   );
 }
